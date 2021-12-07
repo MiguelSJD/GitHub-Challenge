c7f23331733d862909a57be23d6266f5e218736d
MiguelSJD <migueldias101@gmail.com>
2021-12-07 17:34:17 -0300

    [FEATURE] - Create git repository list screen and item repository layout

--- app/src/main/java/br/com/challenge/githubchallenge/ui/main/view/MainActivity.kt
+++ app/src/main/java/br/com/challenge/githubchallenge/ui/main/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.challenge.githubchallenge
+package br.com.challenge.githubchallenge.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.challenge.githubchallenge.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
